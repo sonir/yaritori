0dfc175dc36fadbf465e7737cb774ea6fedc327d
fataris <hana.1995@mac.com>
2017-12-22 17:16:16 +0900

    172222 v0.2, add agent visual

--- DerivedData/yaritori/Build/Intermediates/yaritori.build/Release/yaritori.build/Objects-normal/x86_64/CreEPS.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/addons/ofxVectorGraphics/libs/CreEPS.cpp \
-  ../../../addons/ofxVectorGraphics/libs/CreEPS.hpp \
-  ../../../addons/ofxVectorGraphics/libs/CreEPS_Types.hpp
--- DerivedData/yaritori/Build/Intermediates/yaritori.build/Release/yaritori.build/Objects-normal/x86_64/Event.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/Event.cpp \
-  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/Event.hpp
