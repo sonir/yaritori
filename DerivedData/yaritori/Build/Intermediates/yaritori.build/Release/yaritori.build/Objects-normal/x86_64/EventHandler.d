f5264beb40dd075f19c95d83c617411610946d55
fataris <hana.1995@mac.com>
2017-12-22 15:01:24 +0900

    172222 first commit

---
+++ DerivedData/yaritori/Build/Intermediates/yaritori.build/Release/yaritori.build/Objects-normal/x86_64/EventHandler.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/EventHandler.cpp \
+  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/EventHandler.hpp \
+  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/Event.hpp
---
+++ DerivedData/yaritori/Build/Intermediates/yaritori.build/Release/yaritori.build/Objects-normal/x86_64/ofxGismo.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/ofxGismo.cpp \
+  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/ofxGismo.h \
+  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/agTypes.h \
+  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/Event.hpp \
+  /Users/hana/Development/openFrameworks/of_v0.9.8_osx_release/apps/myApps/yaritori/src/addons/ofxGismo/src/EventHandler.hpp
